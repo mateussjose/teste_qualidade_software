4faf668d9eb4c368fcfa5e452ef19502fa2984ae
Mateus Oliveira <76123045+mateussjose@users.noreply.github.com>
2026-09-24 12:06:37 -0300

    Remove tests for somar and subtrair functions
    Removed tests for somar and subtrair functions.

--- tests/teste_calculadora.py
+++ tests/teste_calculadora.py
@@ -1,11 +1,5 @@
 import pytest
-from src.calculadora import somar, subtrair, multiplicar, dividir
-
-def test_somar():
-    assert somar(2, 3) == 5
-
-def test_subtrair():
-    assert subtrair(5, 2) == 3
+from src.calculadora import multiplicar, dividir
 
 def test_multiplicar():
     assert multiplicar(4, 3) == 12
